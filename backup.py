a5d5b375eeb21be430363d0a00f5fd78dc520b62
denranx <den.ranx@gmail.com>
2014-02-14 08:38:18 +0100

    Fix licence header

--- backup.py
+++ backup.py
@@ -2,12 +2,29 @@
 # -*- mode: Python;-*-
 # -*- coding: utf-8 -*-
 """
-Copyright (C) 2012-2014 pix2d.com
-Author: Laurent Zubiaur
-
-Build a tarball and encrypt using GPG
-
+" Copyright (c) 2013 Laurent Zubiaur
+"
+" http://www.pix2d.com/
+"
+" Permission is hereby granted, free of charge, to any person obtaining a copy
+" of this software and associated documentation files (the "Software"), to deal
+" in the Software without restriction, including without limitation the rights
+" to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+" copies of the Software, and to permit persons to whom the Software is
+" furnished to do so, subject to the following conditions:
+" 
+" The above copyright notice and this permission notice shall be included in
+" all copies or substantial portions of the Software.
+" 
+" THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+" IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+" FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+" AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+" LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+" OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+" THE SOFTWARE.
 """
+
 import sys, os, time, shutil
 from subprocess import call
 import tarfile
@@ -65,7 +82,7 @@ def backup(path):
     # call(["gpg","--symmetric","--cipher-algo","AES256",fname])
 
     # move the backup file to the current directory
-    # shutil.move(tarball,os.getcwd())
+    # shtil.move(tarball,os.getcwd())
 
 def filter_function(tarinfo):
     for pattern in excludes:
